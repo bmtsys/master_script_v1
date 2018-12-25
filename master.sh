afe6d3123e8bdcab8627374539df5172e145625e
Raj Mothe <rajkishan.m@gmail.com>
2018-12-25 08:54:48 +0530

    Updated master.sh
    Modified by Raj

--- master.sh
+++ master.sh
@@ -1,4 +1,23 @@
+
 #!/bin/bash
+
+# Install Pre-requisites ##
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+## Docker Installation ##
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+curl -fsSL https://test.docker.com -o test-docker.sh
+sh test-docker.sh
+
+# Install Docker Compose ##
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Commands ##
 
 echo "The current working directory: $PWD"
 echo "The previous current working directory: $OPLDPWD"
